3673c43d1e2779427b257348fb9c321075589276
Olia <olga.shostak1991@gmail.com>
2020-05-06 19:56:28 +0300

    Operations on an array of objects

--- homework-06/scripts/main.js
+++ homework-06/scripts/main.js
@@ -105,25 +105,4 @@ const getSortedUniqueSkills = users => {
   return [...new Set(skillsNotUnique)];
 };
 
-console.log(getSortedUniqueSkills(users));
-// [
-//   'adipisicing',
-//   'amet',
-//   'anim',
-//   'commodo',
-//   'culpa',
-//   'elit',
-//   'ex',
-//   'ipsum',
-//   'irure',
-//   'laborum',
-//   'lorem',
-//   'mollit',
-//   'non',
-//   'nostrud',
-//   'nulla',
-//   'proident',
-//   'tempor',
-//   'velit',
-//   'veniam',
-// ];
+console.log(getSortedUniqueSkills(users)); // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
